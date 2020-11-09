74d70bf4d451060ae89b5f57afcde4fc18f1f5d1
thearifismail <arifismail@yahoo.com>
2020-11-09 16:49:55 -0500

    Start of schema-validator

---
+++ app/queue/event_consumer.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+from kafka import KafkaProducer
+from kafka import KafkaConsumer
+from kafka.errors import KafkaError
+
+from app.instrumentation import message_not_produced
+from app.instrumentation import message_produced
+from app.logging import get_logger
+
+logger = get_logger(__name__)
+
+Topic = Enum("Topic", ("ingress", "events"))
+
+class EventConsumer:
+    def __init__(self, config):
+        logger.info("Starting EventConsumer()")
+        self._kafka_consumer = KafkaConsumer(**config.kafka_consumer, group_id=config.host_ingress_consumer_group, bootstrap_servers=config.bootstrap_servers)
+        self.topics = {Topic.ingress: config.host_ingress_topic, Topic.events: config.event_topic}
+
+
+    def msg_handler(self, event, key, headers, topic, *, wait=False):
+        print("inside msg_handler()")
+        print("type(event):", type(event))
+        print("event:", event)
+
+    def close(self):
+        self._kafka_consumer.close()
---
+++ lib/schema_validate.py
@@ -0,0 +1,47 @@
+from datetime import timedelta
+
+from app.culling import _Config as CullingConfig
+from app.culling import Timestamps
+from app.queue.event_consumer import Topic
+from app.queue.events import build_event
+from app.queue.events import EventType
+from app.queue.events import message_headers
+from app.queue.queue import EGRESS_HOST_FIELDS
+from app.serialization import serialize_host
+from lib.metrics import synchronize_host_count
+
+__all__ = ("synchronize_hosts",)
+
+WARN_DAYS_AFTER = 7
+DELETE_DAYS_AFTER = 14
+
+
+def validate(select_query, event_consumer, chunk_size, interrupt=lambda: False):
+    start = 0
+    print("Total number: {}".format(select_query.count()))
+    while True:
+        host_list = select_query.offset(start).limit(chunk_size)
+        if host_list.count() <= 0:
+            break
+        for host in host_list:
+            serialized_host = serialize_host(host, _staleness_timestamps(), EGRESS_HOST_FIELDS)
+            event           = build_event(EventType.updated, serialized_host)
+            insights_id     = host.canonical_facts.get("insights_id")
+            headers         = message_headers(EventType.updated, insights_id)
+            event_consumer.msg_handler(event, str(serialized_host), headers, Topic.events, wait=True)
+            synchronize_host_count.inc()
+
+            yield host.id, True
+        start += chunk_size
+
+        # forced stop if needed.
+        if interrupt():
+            return
+
+
+def _staleness_timestamps():
+    cullingConfig = CullingConfig(
+        stale_warning_offset_delta=timedelta(days=WARN_DAYS_AFTER),
+        culled_offset_delta=timedelta(days=DELETE_DAYS_AFTER),
+    )
+    return Timestamps(cullingConfig)
