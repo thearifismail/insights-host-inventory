e7afd2053f0a6d2d9057ea5ca5b188a0d92c5577
thearifismail <arifismail@yahoo.com>
2021-03-16 18:34:05 -0400

    fixed import required for rebasing with upstream master

--- tests/test_api_auth.py
+++ tests/test_api_auth.py
@@ -5,6 +5,7 @@ from app.auth.identity import Identity
 from tests.helpers.api_utils import build_token_auth_header
 from tests.helpers.api_utils import HOST_URL
 from tests.helpers.api_utils import SYSTEM_IDENTITY
+from tests.helpers.api_utils import SYSTEM_PROFILE_URL
 from tests.helpers.test_utils import INSIGHTS_CLASSIC_IDENTITY
 from tests.helpers.test_utils import USER_IDENTITY
 
